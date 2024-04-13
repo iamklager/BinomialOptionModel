185e8afa3d1e384df85609cf271a1b3fa6851c96
Sebastian Rohrer <64891381+Sebroh@users.noreply.github.com>
2024-04-13 21:16:16 +0200

    Update Derivative Pricing.R
    implement American Call and Put Options

--- Derivative Pricing.R
+++ Derivative Pricing.R
@@ -16,7 +16,7 @@ Derivative_Security = function(S, K, N, r, d, u, type){
   option_tree = matrix(0, nrow = N+1, ncol = length(stock_tree))
   deltas = matrix(0, nrow = N, ncol = length(stock_tree)/2)
   
-
+  
   if(type == "European_call"){
     option_tree[nrow(option_tree),] = pmax(stock_tree - K,0)
   }
@@ -54,23 +54,18 @@ Derivative_Security = function(S, K, N, r, d, u, type){
     option_tree[nrow(option_tree),] = pmax(stock_tree - K,0)
     
     for(i in (nrow(option_tree)):2){
-      NO <- (1/(1+r))*(q*option_tree[i, seq(2, ncol(option_tree), by = 2)] + (1-q)*option_tree[i, seq(1, ncol(option_tree), by = 2)])
+      option_tree[i-1, 1:(2^(N-1))] <-
+        (1/(1+r))*(q*option_tree[i, seq(2, ncol(option_tree), by = 2)] + (1-q)*option_tree[i, seq(1, ncol(option_tree), by = 2)])
       
-      print("NO")
-      print(NO)
+      tmp1 <- option_tree[i-1, 1:(2^(N-1))]
+      tmp2 <- rep(0,length(tmp1))
       
-      stock_tree = stock_development(S, i-2, u, d)
+      if((i-1) >= 1){
+        tmp2[1:length(stock_development(S, i-2, u, d))] <- pmax(stock_development(S, i-2, u, d)-K, 0)
+      }
       
-      EX <- pmax(stock_tree - K,0)
-      
-      print("EX")
-      print(EX)
-      
-      
-      option_tree[i-1, 1:(2^(N-1))] <- pmax(EX, NO)
-      
+      option_tree[i-1, 1:(2^(N-1))] <- pmax(tmp1, tmp2)      
       stock_row = stock_development(S, i-1, u, d)
-      
       deltas[i-1, 1:(2^(N-1))] <- (option_tree[i, seq(2, ncol(option_tree), by = 2)] - option_tree[i, seq(1, ncol(option_tree), by = 2)])/(stock_row[seq(2, ncol(option_tree), by = 2)] - stock_row[seq(1, ncol(option_tree), by = 2)])
     }
     
@@ -79,12 +74,36 @@ Derivative_Security = function(S, K, N, r, d, u, type){
     return(list(derivative_prices = option_tree, delta = deltas))
   }    
   
-  
+  else if(type == "American_put"){
+    
+    option_tree[nrow(option_tree),] = pmax(K - stock_tree,0)
+    
+    for(i in (nrow(option_tree)):2){
+      option_tree[i-1, 1:(2^(N-1))] <-
+        (1/(1+r))*(q*option_tree[i, seq(2, ncol(option_tree), by = 2)] + (1-q)*option_tree[i, seq(1, ncol(option_tree), by = 2)])
+      
+      tmp1 <- option_tree[i-1, 1:(2^(N-1))]
+      tmp2 <- rep(0,length(tmp1))
+      
+      if((i-1) >= 1){
+        tmp2[1:length(stock_development(S, i-2, u, d))] <- pmax(K - stock_development(S, i-2, u, d), 0)
+      }
+      
+      option_tree[i-1, 1:(2^(N-1))] <- pmax(tmp1, tmp2)      
+      stock_row = stock_development(S, i-1, u, d)
+      deltas[i-1, 1:(2^(N-1))] <- (option_tree[i, seq(2, ncol(option_tree), by = 2)] - option_tree[i, seq(1, ncol(option_tree), by = 2)])/(stock_row[seq(2, ncol(option_tree), by = 2)] - stock_row[seq(1, ncol(option_tree), by = 2)])
+    }
+    
+    deltas[is.na(deltas)] <- 0
+    
+    return(list(derivative_prices = option_tree, delta = deltas))
+  } 
   
   
   for(i in (nrow(option_tree)):2){
     option_tree[i-1, 1:(2^(N-1))] <-
       (1/(1+r))*(q*option_tree[i, seq(2, ncol(option_tree), by = 2)] + (1-q)*option_tree[i, seq(1, ncol(option_tree), by = 2)])
+    
     stock_row = stock_development(S, i-1, u, d)
     deltas[i-1, 1:(2^(N-1))] <- (option_tree[i, seq(2, ncol(option_tree), by = 2)] - option_tree[i, seq(1, ncol(option_tree), by = 2)])/(stock_row[seq(2, ncol(option_tree), by = 2)] - stock_row[seq(1, ncol(option_tree), by = 2)])
   }
@@ -105,18 +124,10 @@ European_put3 = Derivative_Security(4, 4, 4, (1/4), (1/2), 2, "Asian_call")
 European_put3
 European_put4 = Derivative_Security(4, 4, 4, (1/4), (1/2), 2, "Asian_put")
 European_put4
-European_put5 = Derivative_Security(4, 4, 4, (1/4), (1/2), 2, "American_call")
+European_put5 = Derivative_Security(4, 4, 4, (1/4), (1/2), 2, "American_put")
 European_put5
-
-
-
-
-
-
-
-
-
-
+European_put6 = Derivative_Security(4, 4, 4, (1/4), (1/2), 2, "European_put")
+European_put6
 
 
 
